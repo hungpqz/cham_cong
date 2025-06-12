865e79ae2eafb6f67fa610a8b8141cd62c998363
hungpqz <quanghung.pham@gmail.com>
2025-06-12 15:50:34 +0700

    Update hooks.py

--- cham_cong/hooks.py
+++ cham_cong/hooks.py
@@ -1,9 +1,9 @@
 
 app_name = "cham_cong"
 app_title = "Cham Cong"
-app_publisher = "OpenAI"
+app_publisher = "HungPQ"
 app_description = "Ứng dụng chấm công với dữ liệu từ máy chấm công"
-app_email = "contact@example.com"
+app_email = "hung.pham@wellspringsaigon.edu.vn"
 app_license = "MIT"
 
 fixtures = ["Custom Field"]
